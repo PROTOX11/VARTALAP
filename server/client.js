9c002b2608bc3431f3ac9cf1050fc3fab85bbcec
Prakash kumar <prakashkr2894@gmail.com>
2025-06-21 19:42:29 +0530

    ..

--- server/client.js
+++
@@ -1,7 +0,0 @@
-
-// const { Redis } = require('ioredis');
-
-// const client = new Redis();
-
-// module.exports = client;
-
--- server/config.js
+++
@@ -1,5 +0,0 @@
-// server/config.js
-module.exports = {
-    SECRET_KEY: 'aamkaachar',
-    DB_URI: 'mongodb://localhost:27017/Sample'
-};
