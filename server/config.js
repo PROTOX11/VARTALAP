cc43c76859fd1b0559860a170c0dd1136c265068
Prakash kumar <prakashkr2894@gmail.com>
2025-06-25 15:16:38 +0530

    ..

--- server/config.js
+++ server/config.js
@@ -1,5 +1,5 @@
 // server/config.js
 module.exports = {
   SECRET_KEY: process.env.SECRET_KEY || 'aamkaachar', // Move to environment variable in production
-  DB_URI: process.env.MONGODB_URI || 'mongodb+srv://prakash1142:H49UbrhlzOrUFdeR@cluster0.iyyun4h.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0' // Use a secure password or remove fallback
-};
+  DB_URI: process.env.MONGODB_URI || 'mongodb+srv://prakash1142:H49UbrhlzOrUFdeR@cluster0.iyyun4h.mongodb.net/vartalap?retryWrites=true&w=majority&appName=Cluster0'
+}
