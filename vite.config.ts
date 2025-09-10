bf784cbbacfafe6e990f157ab66c78f7bb8f722a
Prakash kumar <prakashkr2894@gmail.com>
2025-09-10 17:10:41 +0530

    .

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,14 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api': 'https://vartalap-qm32.onrender.com'
-    }
-  },
-  optimizeDeps: {
-    exclude: ['lucide-react'],
+      '/api': {
+        target: 'http://localhost:5000',
+        changeOrigin: true,
+        secure: false,
+      },
+    },
   },
 });
